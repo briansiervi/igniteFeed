a454af10c6b612e2e41bd3bf7e23d8264d8e5ff3
Brian <brianmazini@gmail.com>
2024-06-01 18:14:33 -0300

    tipagem com typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Header } from "./components/Header";
 
 import "./global.css";
 import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -55,14 +55,7 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
